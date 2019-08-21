ea6523b3380d6a568c9901fdf99f3ffebd63418c
Logan Thompson <loganthompson2011@gmail.com>
2019-08-21 11:05:08 -0400

    edits

--- CSE3305_Workspace/main.cpp
+++ CSE3305_Workspace/main.cpp
@@ -1,7 +1,13 @@
+/*Logan Thompson
+ * CSE 3305 S02
+ * B. Kretlow
+ * 8/21/2019
+ */
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
+    //in.get();
 	return 0;
 }
