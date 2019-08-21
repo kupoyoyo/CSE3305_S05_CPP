4296a3a8fdaa2b547d9998f6dc6fe9f98c254c68
Logan Thompson <loganthompson2011@gmail.com>
2019-08-21 18:35:38 -0400

    Added comments to lab0

--- CSE3305_Workspace/Thompson_Lab0.cpp
+++ CSE3305_Workspace/Thompson_Lab0.cpp
@@ -9,28 +9,28 @@
 #include <stdio.h>
 
 void sumOfInts(int num) {
-    if (num > 0) {
-        int sum = 0;
-        for(int i = 1; i <= num; i++) {
+    if (num > 0) { // Will only do the method operation if the integer value is greater than 0
+        int sum = 0; // Declare an integer sum equal to 0
+        for(int i = 1; i <= num; i++) { // Starts a for loop that will run until i reaches the inputted integer value
             if (i == num) {
-                printf ("%i", i);
+                printf ("%i", i); // If i is equal to the inputted integer value, ommit the " + " from the printed string
             }
             else {
-                printf("%i + ", i);
+                printf("%i + ", i); // Prints the current i value with " + " appended onto the string
             }
-            sum += i;
+            sum += i; // Adds the current i value to the sum variable
         }
-        printf(" = %i\n", sum);
+        printf(" = %i\n", sum); // Prints the sum value
     }
     else {
-        printf("The number inputted isn't a positive integer!\n");
+        printf("The number inputted isn't a positive integer!\n"); // Will print to the screen that the number inputted isn't what was expected
     }
 }
 
 int main(int argc, char **argv)
 {
     
-    // Exercise 01 (I'm very sorry for how messy this section is to read)
+    // Exercise 01 (I'm very sorry if this section is messy to read)
     printf("Name: Logan Thompson\n");
     printf("Phones: (404)825-8864\n");
     printf("Email: lthomp92@students.kennesaw.edu\n");
@@ -46,21 +46,25 @@ int main(int argc, char **argv)
     
     // Exercise 02
     printf("\nPlease input a positive integer: ");
-    int numSum;
-    scanf("%i", &numSum);
-    sumOfInts(numSum);
+    int numSum; // Declares integer variable numSum
+    scanf("%i", &numSum); // Read user input for numSum
+    sumOfInts(numSum); // Calls sumOfInts method
     
     // Exercise 03
     printf("\nPlease input another positive integer: ");
-    int numList;
-    scanf("%i", &numList);
-    for(int i = numList; i >= 0; i-=2) {
-        for (int j = i; j >= 0; j-=2) {
-            printf("%i ", j);
-        }
-        printf("\n");
+    int numList; // Declares an integer variable numList
+    scanf("%i", &numList); // Read user input for numList
+    if (numList <= 0) {
+        printf("The number inputted isn't a positive integer!\n"); // Will print to the screen that the number inputted isn't what was expected)
     }
-    
+    else {
+        for(int i = numList; i >= 0; i-=2) { // This nested for loop will print the inputted number and every other number as it decreases to 0, then do the same on the next row decreased by 2 and loop until i is less than 0.
+            for (int j = i; j >= 0; j-=2) { // Sets j equal to the current i value
+                printf("%i ", j); // Will print the current j value
+            }
+            printf("\n"); // Proceeds to next line and i decreases by 2
+        }
+    }
     return 0;
 }
 
