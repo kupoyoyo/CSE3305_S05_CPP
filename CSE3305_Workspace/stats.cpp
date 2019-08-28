6cb7c79c7f0c84cbbc71dafa7801fc11eb30192e
Logan Thompson <loganthompson2011@gmail.com>
2019-08-28 10:48:30 -0400

    Worked on lab 1

---
+++ CSE3305_Workspace/stats.cpp
@@ -0,0 +1,41 @@
+#include "stats.h"
+
+stats :: statistician(int s, int t, int u)
+    : 
+{}
+
+void stats :: next(double r)
+{
+    
+}
+
+void stats :: reset()
+{
+    
+}
+
+int stats :: length() const
+{
+    
+}
+
+double stats :: sum() const
+{
+    
+}
+
+double stats :: mean() const
+{
+    
+}
+
+double stats :: minimum() const
+{
+    
+}
+
+double stats :: maximum() const
+{
+    
+}
+
